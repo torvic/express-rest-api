957a73b02673b706f31c1b3f5b1e44fa9073680e
torvic <130111@unsaac.edu.pe>
2021-12-22 18:08:36 -0500

    Add mongoose and create mongoose connection into app.js

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from 'express'
 import placesRouter from './router/places.routes.js'
 import usersRouter from './router/users.routes.js'
+import mongoose from 'mongoose'
+import 'colors'
 // Utils
-import HttpError from './models/http-error.model.js';
+import HttpError from './models/http-error.model.js'
 
+// connection to mongoDB
+const MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/places_app'
+
+mongoose
+  .connect(MONGO_URI)
+  .then((connection) =>
+    console.log(
+      `MongoDb Connected: ${connection.connection.host}`.cyan.underline.bold
+    )
+  )
+  .catch((error) => console.log(`MongoDB connection error: ${error}`.red.bold))
 
 // initialize express
 const app = express()
@@ -15,8 +29,8 @@ app.use((error, req, res, next) => {
   if (res.headersSent) {
     return next(error)
   }
-	if (error instanceof Result) {
-    return res.status(400).json({ errors: error.array() });
+  if (error instanceof Result) {
+    return res.status(400).json({ errors: error.array() })
   }
   res.status(error.code || 500)
   res.json({ message: error.messages || 'An unknown error occurred!' })
@@ -27,7 +41,7 @@ app.use('/api/places', placesRouter)
 app.use('/api/users', usersRouter)
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this routes.', 404);
-  throw error;
-});
+  const error = new HttpError('Could not find this routes.', 404)
+  throw error
+})
 app.listen(PORT, console.log(`Server is running on PORT: ${PORT}`))
