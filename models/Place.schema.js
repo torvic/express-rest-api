2061f0b9c77b32cc36f52db10bf937a3afeb6dfb
torvic <130111@unsaac.edu.pe>
2021-12-22 18:10:41 -0500

    Create Place schema

---
+++ models/Place.schema.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const placeSchema = new Schema({
+  title: { type: String, required: true },
+  description: {
+    type: String,
+    required: [true, 'Please provide a description.'],
+  },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: String, required: true },
+});
+
+export default model('Place', placeSchema);
