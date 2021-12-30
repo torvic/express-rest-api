7e1a194417a7bb71fae7d488cbe4f9c16f3e4c66
torvic <130111@unsaac.edu.pe>
2021-12-30 12:35:49 -0500

    Create User schema

---
+++ models/User.schema.js
@@ -0,0 +1,13 @@
+// import mongoose from 'mongoose';
+// const { Schema, model } = mongoose;
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true },
+});
+
+export default model('User', userSchema);
