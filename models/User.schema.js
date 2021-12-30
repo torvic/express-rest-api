74c279f31d365878d39498f54d6365a21a7546e6
torvic <130111@unsaac.edu.pe>
2021-12-30 12:39:58 -0500

    Using custom async validator for unique email in User schema

--- models/User.schema.js
+++ models/User.schema.js
@@ -1,16 +1,33 @@
-// import mongoose from 'mongoose';
-// const { Schema, model } = mongoose;
-import { Schema, model, plugin } from 'mongoose';
-import uniqueValidator from 'mongoose-unique-validator';
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+// import { Schema, model, plugin } from 'mongoose';
+// import uniqueValidator from 'mongoose-unique-validator';
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    validate: {
+      validator: async function (email) {
+        const user = await this.constructor.findOne({ email });
+        if (user) {
+          if (this.id === user.id) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      },
+      message: 'The specified email address is already in use.',
+    },
+    required: [true, 'User email required.'],
+    unique: true,
+  },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
   places: { type: String, required: true },
 });
 
-plugin(uniqueValidator);
+// plugin(uniqueValidator);
 
 export default model('User', userSchema);
